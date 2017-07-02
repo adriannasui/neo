f800246196f308897fec801f5aaf332f9dc6e73a
AshRolls <devchan@antshares_slack>
2017-07-02 14:36:02 +0100

    new project for unit tests, simple interface, and two basic tests

---
+++ src/AntShares/Core/IAccountState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using AntShares.Cryptography.ECC;
+
+namespace AntShares.Core
+{
+    public interface IAccountState
+    {
+        Dictionary<UInt256, Fixed8> Balances { get; set; }
+        bool IsFrozen { get; set; }
+        UInt160 ScriptHash { get; set; }        
+        ECPoint[] Votes { get; set; }
+        int Size { get; }
+
+        void Deserialize(BinaryReader reader);
+        void Serialize(BinaryWriter writer);
+    }
+}
