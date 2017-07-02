7c720aaf6fc80fba658d5402a3da9ecbe7d37c98
AshRolls <devchan@antshares_slack>
2017-07-02 16:56:40 +0100

    a static container we can call when we want to inject class into interface

---
+++ src/AntShares/Bindings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SimpleInjector;
+using AntShares.Core;
+
+namespace AntShares
+{
+    public class Bindings
+    {
+        static Container Container_Var;
+        static Object Container_Lock = new Object();
+        public static Container Container
+        {
+            get
+            {
+                lock (Container_Lock)
+                {
+                    if (null == Container_Var)
+                    {
+                        Container_Var = GetInjectorContainer();
+                    }
+                }
+                return Container_Var;
+            }
+
+            private set
+            {
+                Container_Var = value;
+            }
+        }
+
+        private static Container GetInjectorContainer()
+        {
+            Container container = new Container();
+
+            // Core
+            container.Register<IAccountState, AccountState>();
+
+            // optional, verify
+            container.Verify();
+
+            return container;
+        }
+    }
+}
